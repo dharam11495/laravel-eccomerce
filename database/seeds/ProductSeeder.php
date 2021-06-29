510f8a2775d31b17a244e03632a44d3c27dc2a2e
dharamveer <dharamveer.aipl@gmail.com>
2021-06-29 17:07:05 +0530

    add product table

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+Use Illuminate\Support\Facades\DB;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('product')->insert([
+            [
+            'name' => 'L G TV',
+             'price' => '15999',
+             'description' => 'test description lorem spum do it work test description',
+             'category' => 'tv'
+            ],
+            [
+                'name' => 'Nokia mobile',
+                 'price' => '7000',
+                 'description' => 'test description lorem spum do it work test description',
+                 'category' => 'mobile'
+            ],
+            [
+                'name' => 'I phone',
+                 'price' => '83599',
+                 'description' => 'test description lorem spum do it work test description',
+                 'category' => 'mobile'
+            ],
+            [
+                'name' => 'LED',
+                 'price' => '83599',
+                 'description' => 'test description lorem spum do it work test description',
+                 'category' => 'tv'
+            ]
+        ]);
+    }
+}
